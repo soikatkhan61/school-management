aea4d9281abfbce42bf3edadb4c04c7c2fa80581
soikatkhan61 <soikatkhan61@gmail.com>
2023-04-07 03:38:09 +0600

    fix utils

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
+const Flash = require("../utils/Flash")
 
 router.get("/",(req,res)=>{
-    res.render('index',{flasMessage:''})
+    res.render('index',{flashMessage:Flash.getMessage(req)})
 })
 
 module.exports = router
